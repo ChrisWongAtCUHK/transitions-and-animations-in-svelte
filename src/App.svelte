2473b9050a31ed01869b9f4817555ab63a92b31b
Chris Wong <wc38.wong@gmail.com>
2023-06-30 19:50:52 +0800

    Transitions and Animations in Svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,38 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import { fade, slide, fly } from 'svelte/transition'
+  import { elasticOut } from 'svelte/easing'
+  let showing = false
+
+  function custom(node, params) {
+    return {
+      easing: params.easing,
+      css: (t, u) => `transform: translatex(${u * 200}px) scale(${t})`
+    }
+  }
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <label>
+    Showing
+    <input bind:checked={showing} type="checkbox" />
+  </label>
+  {#if showing}
+    <div transition:custom={{ easing: elasticOut }} />
+  {/if}
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
+  main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 4rem;
+    padding-block-start: 10rem;
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
+
+  div {
+    height: 200px;
+    width: 200px;
+    background: goldenrod;
   }
 </style>
